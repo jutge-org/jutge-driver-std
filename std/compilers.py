9830646f2df89398c0368f93e2e20cae003fc730
Jutge.org Labs <contact@jutge.org>
2022-05-27 12:06:22 +0200

    Fix Python3 compiler problems

--- std/compilers.py
+++ std/compilers.py
@@ -8,7 +8,6 @@ import signal
 import subprocess
 import time
 import shutil
-import turtle_pil
 
 import monitor
 from jutge import util
@@ -1558,16 +1557,17 @@ class Compiler_Python3(Compiler):
         return util.file_size('compilation2.txt') == 0
 
     def execute(self, tst):
-        # Under vinga, python cannot locate the modules in the current dir, so we move them to a subdir.
-        util.copy_file("../../driver/etc/py/python_wrapper.py", "./wrapper.py")
-        os.mkdir('subdir')
-        util.copy_file('program.py', 'subdir')
-        # hack to use turtle-pil when using turtle
-        shutil.copy(turtle_pil.__file__, 'subdir/turtle.py')
+        # under jutge-vinga, for some strange reason, python cannot locate the modules in the current dir, so we move them to a subdir under /tmp.
 
-        self.execute_monitor(tst, '/usr/bin/env python3 wrapper.py')
-
-        util.del_file('subdir/turtle.py')
+        ori = os.getcwd()
+        wrk = '/tmp/' + tst + '.workdir'
+        util.del_dir(wrk)
+        os.mkdir(wrk)
+        os.system('cp -r * ' + wrk)
+        os.chdir(wrk)
+        self.execute_monitor(tst, '/usr/bin/python3 program.py')
+        os.system('cp -r * ' + ori)
+        os.chdir(ori)
 
 
 class Compiler_Perl(Compiler):
